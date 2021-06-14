8f0a0dd40c351dcfd5b4c780c42889ded1d8446b
Cihan Kaya <cihankaya42@gmail.com>
2021-06-14 20:41:42 +0300

    only BE files

--- README.md
+++ README.md
@@ -4,15 +4,9 @@
 ### Configuration
 - **Platform:** node
 - **Framework**: express
-- **Template Engine**: jade
-- **CSS Framework**: none
-- **CSS Preprocessor**: css
-- **JavaScript Framework**: react
 - **Build Tool**: npm
 - **Unit Testing**: mocha
 - **Database**: postgresql
 - **Authentication**: email,facebook,google,twitter
 - **Deployment**: none
 
-### License
-The MIT License (MIT)
--- server.js
+++ server.js
@@ -3,16 +3,9 @@ var path = require('path');
 var logger = require('morgan');
 var compression = require('compression');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var dotenv = require('dotenv');
-var React = require('react');
-var ReactDOM = require('react-dom/server');
-var Router = require('react-router');
-var Provider = require('react-redux').Provider;
 var jwt = require('jsonwebtoken');
-var moment = require('moment');
-var request = require('request');
 
 // Load environment variables from .env file
 dotenv.load();
@@ -28,22 +21,16 @@ var User = require('./models/User');
 var userController = require('./controllers/user');
 var contactController = require('./controllers/contact');
 
-// React and Server-Side Rendering
-var routes = require('./app/routes');
-var configureStore = require('./app/store/configureStore').default;
 
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3300);
 app.use(compression());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   req.isAuthenticated = function() {
@@ -83,33 +70,6 @@ app.get('/auth/google/callback', userController.authGoogleCallback);
 app.post('/auth/twitter', userController.authTwitter);
 app.get('/auth/twitter/callback', userController.authTwitterCallback);
 
-// React server rendering
-app.use(function(req, res) {
-  var initialState = {
-    auth: { token: req.cookies.token, user: req.user },
-    messages: {}
-  };
-
-  var store = configureStore(initialState);
-
-  Router.match({ routes: routes.default(store), location: req.url }, function(err, redirectLocation, renderProps) {
-    if (err) {
-      res.status(500).send(err.message);
-    } else if (redirectLocation) {
-      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search);
-    } else if (renderProps) {
-      var html = ReactDOM.renderToString(React.createElement(Provider, { store: store },
-        React.createElement(Router.RouterContext, renderProps)
-      ));
-      res.render('layout', {
-        html: html,
-        initialState: store.getState()
-      });
-    } else {
-      res.sendStatus(404);
-    }
-  });
-});
 
 // Production error handler
 if (app.get('env') === 'production') {
