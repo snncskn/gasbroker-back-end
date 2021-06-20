b4f21bde70371da88b5a894839b93a50388970aa
Sinan <snncskn@macbook.local>
2021-06-20 15:03:07 +0300

    Security off all of them

--- src/company/router.js
+++ src/company/router.js
@@ -7,10 +7,10 @@ const { authJwt } = require("../../auth/middleware");
 // parametre ile karışma ihtimali olanlar varsa onları daha ekle
 // requestlerde mümkün olduğunca next kullanma emin olmadıkça
 router.get('/check', controller.check)
-router.get('/sql/all/', [authJwt.verifyToken, authJwt.isAdmin], controller.getAllBySql)
+router.get('/sql/all/', controller.getAllBySql)
 router.get('/sql/:company_id', controller.getByIdBySql)
-router.put('/delete/:company_id', [authJwt.verifyToken, authJwt.isModeratorOrAdmin], controller.delete)
-router.put('/changeactive/:company_id', [authJwt.verifyToken, authJwt.isModeratorOrAdmin], controller.changeActiveStatus)
+router.put('/delete/:company_id', controller.delete)
+router.put('/changeactive/:company_id', controller.changeActiveStatus)
 router.get('/:company_id', controller.getByID)
 router.put('/:company_id', controller.update)
 router.get('/', controller.getAll)
