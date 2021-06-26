2f0c479487d42052736d74765600d81c99153456
Sinan COSKUN <snncskn@msn.com>
2021-06-26 21:57:54 +0300

    push

--- models/index.js
+++ models/index.js
@@ -44,14 +44,14 @@ db.role.belongsToMany(db.user, { as: 'userId_users', through: db.user_roles, for
 db.user.belongsToMany(db.role, { as: 'roleId_roles', through: db.user_roles, foreignKey: "userId", otherKey: "roleId" });
 db.address.belongsTo(db.company, { as: "company", foreignKey: "company_id"});
 db.company.hasMany(db.address, { as: "addresses", foreignKey: "company_id"});
-db.company_type.belongsTo(db.company, { as: "company", foreignKey: "company_id"});
-db.company.hasMany(db.company_type, { as: "company_types", foreignKey: "company_id"});
+db.company_types.belongsTo(db.company, { as: "company", foreignKey: "company_id"});
+db.company.hasMany(db.company_types, { as: "company_types", foreignKey: "company_id"});
 db.vehicle.belongsTo(db.company, { as: "company", foreignKey: "company_id"});
 db.company.hasMany(db.vehicle, { as: "vehicles", foreignKey: "company_id"});
 db.user_roles.belongsTo(db.role, { as: "role", foreignKey: "roleId"});
 db.role.hasMany(db.user_roles, { as: "user_roles", foreignKey: "roleId"});
-db.company_type.belongsTo(db.type, { as: "type", foreignKey: "type_id"});
-db.type.hasMany(db.company_type, { as: "company_types", foreignKey: "type_id"});
+db.company_types.belongsTo(db.type, { as: "type", foreignKey: "type_id"});
+db.type.hasMany(db.company_types, { as: "company_types", foreignKey: "type_id"});
 db.user_roles.belongsTo(db.user, { as: "user", foreignKey: "userId"});
 db.user.hasMany(db.user_roles, { as: "user_roles", foreignKey: "userId"});
 
