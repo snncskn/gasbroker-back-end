7459502f7aad749b1552394c0c667e8d99fa3339
Sinan COSKUN <snncskn@msn.com>
2021-07-10 16:04:48 +0300

    proposal offer

---
+++ models/proposal_offer.js
@@ -0,0 +1,65 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('proposal_offer', {
+    id: {
+      defaultValue: DataTypes.UUIDV4,
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      references: {
+        model: 'proposal',
+        key: 'id'
+      }
+    },
+    proposal_id: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    offer_date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    payment_type: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: true
+    },
+    deal_status: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'proposal_offer',
+    schema: 'public',
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    deletedAt: 'deleted_at',
+    paranoid: true,
+    timestamps: true,
+    indexes: [
+      {
+        name: "proposal_offer_pk",
+        unique: true,
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
