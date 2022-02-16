014fbbaaade4b76e9c9e31e16b2ae7fddb54b29d
Uzeyr OZCAN <muzeyr@gmail.com>
2022-02-16 14:44:06 +0300

    user message

--- src/message/router.js
+++ src/message/router.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const controller = require("./controller");
 
 router.put("/delete/:message_id", controller.delete);
+router.get("/user", controller.getMessagesByUserlId);
 router.get("/:message_id", controller.getById);
 router.put("/:message_id", controller.update);
 router.get("/", controller.getAll);
 router.post("/", controller.create);
 
 router.get("/proposal/:proposal_id", controller.getByProposalId);
-router.get("/user/", controller.getMessagesByUserlId);
 
 module.exports = router;
